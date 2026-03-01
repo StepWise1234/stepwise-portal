b634d8cb8964197095124123d0ece72e2ab06068
Laela Leonard <minihome@Macmini.lan>
2026-03-01 03:15:21 -0800

    Force new build to clear CDN cache

--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useCallback } from 'react'
+import { useState, useMemo, useCallback } from 'react' // Updated
 import { Link, useNavigate } from 'react-router-dom'
 import { format, formatDistanceToNow } from 'date-fns'
 import {
